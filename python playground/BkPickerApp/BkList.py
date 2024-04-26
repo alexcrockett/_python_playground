fa64ba6a0655c2626510090a3f75324e25640ff2
Alex Crockett <alexandercrockett@gmail.com>
2024-04-25 22:27:13 -0700

    Edited book class and added class for library records with functions.

--- python playground/BkPickerApp/BkList.py
+++
@@ -1,5 +0,0 @@
-import LibraryClasses
-
-
-def libGUI():
-# Welcome the user and ask them what they want to do
---
+++ python playground/BkPickerApp/BookApp.py
@@ -0,0 +1,11 @@
+import LibraryClasses
+
+def lib_ui():
+	welcome_msg = input("Welcome to the Library, press 1 to add a book and 2 search for a book")
+	if welcome_msg == "1":
+
+
+def book_submit
+
+
+def search_submit
