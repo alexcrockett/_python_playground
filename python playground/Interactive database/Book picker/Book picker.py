9d382fba6a19f4aae7546c58a015ce30419eeb8b
Alex Crockett <alexandercrockett@gmail.com>
2024-04-25 21:00:49 -0700

    Edited book class and added class for library records with functions.

--- python playground/Interactive database/Book picker/Book picker.py
+++ python playground/Interactive database/Book picker/Book picker.py
@@ -12,4 +12,25 @@ class BOOK:
 		self.author = author
 
 	def __str__(self):
-		return f"{self.title}', genre = '{self.genre}"
+		return f"{self.title}', genre = '{self.genre}', author = '{self.author}"
+
+
+class Library:
+	def __init__(self):
+		self.books = []  # This will store instances of BOOK
+
+	def add_book(self, title, genre, author):
+		# Create a new BOOK instance and append it to the list
+		new_book = BOOK(title, genre, author)
+		self.books.append(new_book)
+
+	def list_books(self):
+		# Print all books in the library
+		for book in self.books:
+			print(book)
+
+	def find_books_by_genre(self, genre):
+		# Find and print books by specific genre
+		found_books = [book for book in self.books if genre in book.genre]
+		for book in found_books:
+			print(book)
