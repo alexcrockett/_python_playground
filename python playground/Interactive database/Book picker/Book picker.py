63f225207a858189c71c55a92126db0a21faae12
Alex Crockett <alexandercrockett@gmail.com>
2024-04-25 21:58:32 -0700

    Edited book class and added class for library records with functions.

--- python playground/Interactive database/Book picker/Book picker.py
+++ python playground/Interactive database/Book picker/Book picker.py
@@ -3,26 +3,26 @@ This program will store a user's reading list and make reading suggestions from 
 book titles.
 """
 
-
-# The book class
 class BOOK:
-	def __init__(self, title, genre, author):
+	def __init__(self, title, genres, author):  # Assuming genres can be a list of genres
 		self.title = title
-		self.genre = genre
+		self.genres = genres  # Changed to list handling
 		self.author = author
 
 	def __str__(self):
-		return f"{self.title}', genre = '{self.genre}', author = '{self.author}"
-
+		return f"{self.title}, genres = '{', '.join(self.genres)}', author = '{self.author}"
 
 class Library:
 	def __init__(self):
 		self.books = []  # This will store instances of BOOK
 
-	def add_book(self, title, genre, author):
-		# Create a new BOOK instance and append it to the list
-		new_book = BOOK(title, genre, author)
-		self.books.append(new_book)
+	def add_book(self, title, genres, author):
+		# Check for duplicates
+		if not any(book.title == title and book.author == author for book in self.books):
+			new_book = BOOK(title, genres, author)
+			self.books.append(new_book)
+		else:
+			print("This book is already in your library.")
 
 	def list_books(self):
 		# Print all books in the library
@@ -31,6 +31,8 @@ class Library:
 
 	def find_books_by_genre(self, genre):
 		# Find and print books by specific genre
-		found_books = [book for book in self.books if genre in book.genre]
+		found_books = [book for book in self.books if genre in book.genres]
 		for book in found_books:
 			print(book)
+
+
