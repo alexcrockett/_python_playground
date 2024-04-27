a237389c5e0ffcbe1aa91891ef50f87ad1baf54c
Alex Crockett <alexandercrockett@gmail.com>
2024-04-26 23:42:44 -0700

    Started writing functions to add books to library.

--- python playground/BkPickerApp/BookApp.py
+++ python playground/BkPickerApp/BookApp.py
@@ -1,11 +1,42 @@
 import LibraryClasses
+import BkList
+
 
 def lib_ui():
 	welcome_msg = input("Welcome to the Library, press 1 to add a book and 2 search for a book")
 	if welcome_msg == "1":
+		return book_title()
+	if welcome_msg == "2":
+		return
+	else:
+		second_attempt()
 
 
-def book_submit
+def second_attempt():
+	attempt_msg = input("Would you like to exit the Library, press q to quit and any other key to continue")
+	if attempt_msg.lower() == "q":
+		exit()
+	else:
+		lib_ui()
 
 
-def search_submit
+def book_title():
+	title = LibraryClasses.BOOK.title = input("Please enter a book title")
+	# confirm the book title and then move to genre
+	return title_check(title)
+
+
+def title_check(title):
+	check_title = input("You entered: " + title + ". Would you like to proceed? y for yes, n for no")
+	if check_title.lower() == "y":
+		return book_genre()
+	else:
+		print("Press 1 to start again and press q to quit")
+		if check_title.lower() == "q":
+			exit()
+		else:
+			book_title()
+
+
+def book_genre():
+	genre = LibraryClasses.BOOK.genre = input("Please enter a book genre")
