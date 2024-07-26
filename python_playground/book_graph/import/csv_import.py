60fe4852570e009f4a07f11e735879428d7c7b71
Alex Crockett <alexandercrockett@gmail.com>
2024-07-26 01:24:33 -0700

    Changes to data-flow and logic.

--- python_playground/book_graph/import/csv_import.py
+++ python_playground/book_graph/import/csv_import.py
@@ -1,28 +1,25 @@
 class CSVIMPORTER:
 	def __init__(self):
 		self.source_path = '../databases/raw_library.csv'
+		self.df = None
 
 	def read_csv(self):
 		import pandas as pd
-		df = pd.read_csv(self.source_path)
+		self.df = pd.read_csv(self.source_path)
 
 
 class CSVFRAME(CSVIMPORTER):
-	def data_frame(self, df):
-		new_frame = df[[
-			'Title',
-			'type',
-			'Author',
-			'Additional Authors',
-			'ISBN',
-			'ISBN13',
-			'Publisher',
-			'Number of Pages',
-			'Original Publication Year',
-			'Summary',
-			'Tags',
-			'Genres',
-			'Text',
-			'Linked Data'
-		]].copy()
-		return new_frame
+	def data_frame(self):
+		if self.df is not None:
+			# List the columns you want to select
+			columns_to_select = [
+				'Title', 'type', 'Author', 'Additional Authors', 'ISBN',
+				'ISBN13', 'Publisher', 'Number of Pages', 'Original Publication Year',
+				'Summary', 'Tags', 'Genres', 'Text', 'Linked Data'
+			]
+			# Use the list to select columns directly
+			new_frame = self.df[columns_to_select].copy()
+			return new_frame
+		else:
+			print("Data not loaded. Check CSV and try again.")
+			return None
